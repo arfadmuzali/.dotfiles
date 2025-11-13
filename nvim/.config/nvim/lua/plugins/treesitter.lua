082e901aa6034fcae4f90078631826f6a3839275
arfadmuzali <arfadmuzali258@gmail.com>
2025-11-13 21:36:16 +0700

    update neovim and kitty colors

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+        opts = {
+            ensure_installed = {
+                'bash',
+                'c',
+                'diff',
+                'html',
+                'lua',
+                'go',
+                'luadoc',
+                'markdown',
+                'markdown_inline',
+                'query',
+                'vim',
+                'vimdoc',
+            },
+            -- Autoinstall languages that are not installed
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+        },
+    },
+}
